acf3def01883dc081361b306e8109669682e79c1
Rolly Mbaki <rollymbakii@gmail.com>
2025-03-01 17:27:54 +0100

    tested

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import { ArrowUp, Loader, X } from "lucide-react"
 import { generateContent, generateDeenBuddy } from "./components/Model"
 
