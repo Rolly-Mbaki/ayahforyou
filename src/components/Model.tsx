d2ebc4cdaf07e498abd5e4e1b0dfcb9968fdc928
Rolly-Mbaki <rollymbakii@gmail.com>
2025-12-04 20:23:43 +0100

    changing from geminin 1.5 to 2.5

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,36 +1,21 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
 import { genZ, scholar } from "./ModelTexts";
+import { GoogleGenAI } from "@google/genai";
 
-const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ 
-    model: "gemini-1.5-flash",
-    systemInstruction: genZ 
-});
-const chat = model.startChat({
-    history: [
-      {
-        role: "user",
-        parts: [{ text: "Hello" }],
-      },
-      {
-        role: "model",
-        parts: [{ text: "Great to meet you. What would you like to know?" }],
-      },
-    ],
-  });
-  
+const genAI = new GoogleGenAI({apiKey: import.meta.env.VITE_GEMINI_API_KEY});
 
-export const generateContent = async (prompt:string): Promise<string> => {
-    const result = await chat.sendMessage(prompt)
-    return result.response.text(); // return the response
-}
+// const model = genAI.models.generateContent({
+//     model: "gemini-2.5-flash",
+//     contents:"Hello there",
+//     config: {
+//       systemInstruction: genZ
+//     }
+// });
 
-const scholarModel = genAI.getGenerativeModel({ 
-  model: "gemini-1.5-flash",
-  systemInstruction: scholar 
-});
-
-export const scholarChat = scholarModel.startChat({
+const chat = genAI.chats.create({
+  model: "gemini-2.5-flash",
+  config: {
+    systemInstruction: genZ,
+  },
   history: [
     {
       role: "user",
@@ -43,7 +28,47 @@ export const scholarChat = scholarModel.startChat({
   ],
 });
 
-export const generateDeenBuddy = async (prompt:string): Promise<string> => {
-  const result = await chat.sendMessage(prompt)
-  return result.response.text(); // return the response
-}
+export const generateContent = async (prompt: string): Promise<string> => {
+  const response = await chat.sendMessage({ message: prompt });
+
+  if (response.text == undefined) {
+    return "error";
+  }
+
+  return response.text!; // return the response
+};
+
+// const scholarModel = genAI.models.generateContent({
+//   model: "gemini-1.5-flash",
+//   contents: "Hello there",
+//   config: {
+//     systemInstruction: scholar,
+//   },
+// });
+
+export const scholarChat = genAI.chats.create({
+  model: "gemini-1.5-flash",
+  config: {
+    systemInstruction: scholar,
+  },
+  history: [
+    {
+      role: "user",
+      parts: [{ text: "Hello" }],
+    },
+    {
+      role: "model",
+      parts: [{ text: "Great to meet you. What would you like to know?" }],
+    },
+  ],
+});
+
+export const generateDeenBuddy = async (prompt: string): Promise<string> => {
+  const response = await chat.sendMessage({ message: prompt });
+
+  if (response.text == undefined) {
+    return "error";
+  }
+
+  return response.text; // return the response
+};
